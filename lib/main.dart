91d55a12d83f0c41ca578c6ad811e4217df499bc
Aidai <aydaysharipowa@icloud.com>
2026-04-14 10:28:59 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+// screens
+import 'features/map/map_screen.dart';
+import 'features/game/game_screen.dart';
+import 'features/chat/chat_screen.dart';
+import 'features/stats/stats_screen.dart';
+import 'features/profile/profile_screen.dart';
+
+void main() {
+  runApp(const EcoSortApp());
+}
+
+class EcoSortApp extends StatelessWidget {
+  const EcoSortApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'EcoSort App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _index = 0;
+
+  final List<Widget> pages = const [
+    MapScreen(),
+    GameScreen(),
+    ChatScreen(),
+    StatsScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _index,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        onTap: (value) {
+          setState(() {
+            _index = value;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: "Map",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.gamepad),
+            label: "Game",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: "Chat",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: "Stats",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
+  }
+}
